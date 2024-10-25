974175b91954a0e4e3f489c3c688225d2663b470
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-25 11:01:03 -0700

    Create a ModelSerializer

---
+++ LittleLemon/LittleLemonDRF/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import MenuItem, Category
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'title']
+
