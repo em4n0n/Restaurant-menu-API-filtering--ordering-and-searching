87a456690f57b4eade8418ac6836a5e36e0e45d1
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-25 11:14:41 -0700

    Add pagination classes

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_CLASSES': [
+        'rest_framework.filters.OrderingFilter',
+        'rest_framework.filters.SerachFilter',
+        ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
+    
+}
