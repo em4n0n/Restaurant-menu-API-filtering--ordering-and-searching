81aff581b054030f4bce95f35f51c0fae5072b68
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-25 13:25:27 -0700

    fix title field

--- LittleLemon/LittleLemonDRF/models.py
+++ LittleLemon/LittleLemonDRF/models.py
@@ -10,9 +10,9 @@ class Category(models.Model):
         return self.title
     
 class MenuItem(models.Model):
-    title = models.SlugField()
-    price = models.CharField(max_length=255)
-    inventory = models.SmallIntegerField(max_digits=6, decimal_places=2)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT, default=1)
     
     def __str__(self)-> str:
