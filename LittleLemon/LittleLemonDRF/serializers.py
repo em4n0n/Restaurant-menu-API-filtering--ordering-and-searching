46ed1d0d8c2db9887cdf38e96d5dd833280dd37a
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-25 11:07:16 -0700

    Create MeniItemSerializers

--- LittleLemon/LittleLemonDRF/serializers.py
+++ LittleLemon/LittleLemonDRF/serializers.py
@@ -6,3 +6,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'title']
 
+class MenuItemSerializer(serializers.ModelSerializer):
+    category_id = serializers.IntegerField(write_only=True)
+    category = CategorySerializer(read_only=True)
+    
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'inventory', 'category', 'category_id']
+        
