c33c57a63794de0f5b43468404c9707dd1853dd2
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-10-25 14:01:18 -0700

    Register url path

--- LittleLemon/LittleLemonDRF/urls.py
+++ LittleLemon/LittleLemonDRF/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('category', views.CategoriesView.as_view()),
     path('menu-items', views.MenuItemsView.as_view()),
+    path('menu-items/<int:pk>/', views.MenuItemDetailView.as_view()),
 ]
